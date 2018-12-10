61d156375b704aa3c8ec725f52e5e8c98feedb3f
scotty30750 <43013351+scotty30750@users.noreply.github.com>
2018-12-10 15:38:57 -0500

    added various comments to my code

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
-var img;
-var anthem;
+var img;//establishing image variable of blank American flag so it can be later colored in
+var anthem;//establishing sound variable so it can play (USA national anthem) while coloring in flag
 function preload() {
   img = loadImage('americanflag.gif');
   anthem = loadSound('USAanthem.mp3');
-}
+}//function loading my image and mp3 file of national anthem
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(600, 600);//create canvas for my flag to appear on
     image(img, 0, 0);
 }
 function draw() {
   stroke(100);
   if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);//function- if the mouse is pressed then it will draw lines
   }
 }
 function mouseClicked() {
@@ -26,5 +26,5 @@ function mousePressed() {
     anthem.stop();
   } else {
     anthem.play();
-  }
+  }//if the mouse is pressed then the USA national anthem will be played simultaneously while coloring in American flag :) !
 }
