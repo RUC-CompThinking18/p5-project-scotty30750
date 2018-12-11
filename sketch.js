53ab22fd2f34d9050c5877404882794bd20a611f
scotty30750 <43013351+scotty30750@users.noreply.github.com>
2018-12-11 16:25:39 -0500

    added color choice, red and blue

--- sketch.js
+++ sketch.js
@@ -1,19 +1,44 @@
 var img;//establishing image variable of blank American flag so it can be later colored in
 var anthem;//establishing sound variable so it can play (USA national anthem) while coloring in flag
+var colornow;
 function preload() {
   img = loadImage('americanflag.gif');
   anthem = loadSound('USAanthem.mp3');
 }//function loading my image and mp3 file of national anthem
 function setup() {
-    createCanvas(600, 600);//create canvas for my flag to appear on
+    createCanvas(602, 600);//create canvas for my flag to appear on
     image(img, 0, 0);
+    colornow = color("red");
 }
 function draw() {
-  stroke(100);
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);//function- if the mouse is pressed then it will draw lines
+  stroke("black");
+  noFill();
+  if (mouseIsPressed && mouseX < 571) {
+    push();
+    stroke(colornow);
+    fill(colornow);
+    /*
+    ellipse(mouseX, mouseY, 4);//function- if the mouse is pressed then it will draw lines
+    */
+    strokeWeight(4);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    pop();
   }
+  else if (mouseIsPressed && mouseX >= 571 ){
+    var pixel = get(mouseX, mouseY);
+    console.log(pixel);
+    colornow = color(pixel[0], pixel[1], pixel[2])
+  }
+  push();
+  stroke('black');
+  fill('blue');
+  rect(570, 0, 30, 30);
+  fill('red');
+  rect(570, 30, 30, 30);
+  pop();
+
 }
+/*
 function mouseClicked() {
   if (value === 0) {
     value = 100;
@@ -21,6 +46,7 @@ function mouseClicked() {
     value = 0;
   }
 }
+*/
 function mousePressed() {
   if ( anthem.isPlaying() ) {
     anthem.stop();
